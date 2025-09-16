3c63a1b5c1399e4f7830e7ad5a33e52f07da582f
absallam1999 <absallam1999@gmail.com>
2025-09-16 16:23:35 +0300

    Payments

--- Core/DTO/PaymentResponse/PaypalPaymentResponse.cs
+++ Core/DTO/PaymentResponse/PaypalPaymentResponse.cs
@@ -5,10 +5,10 @@ namespace invoice.Core.DTO.PaymentResponse
     public class PayPalAuthResponse
     {
         [JsonPropertyName("access_token")]
-        public string AccessToken { get; set; }
+        public string? AccessToken { get; set; }
 
         [JsonPropertyName("token_type")]
-        public string TokenType { get; set; }
+        public string? TokenType { get; set; }
 
         [JsonPropertyName("expires_in")]
         public int ExpiresIn { get; set; }
@@ -17,24 +17,48 @@ namespace invoice.Core.DTO.PaymentResponse
     public class PayPalOrderResponse
     {
         [JsonPropertyName("id")]
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
         [JsonPropertyName("status")]
-        public string Status { get; set; }
+        public string? Status { get; set; }
 
         [JsonPropertyName("links")]
-        public List<PayPalLink> Links { get; set; }
+        public List<PayPalLink>? Links { get; set; }
     }
 
     public class PayPalLink
     {
         [JsonPropertyName("href")]
-        public string Href { get; set; }
+        public string? Href { get; set; }
 
         [JsonPropertyName("rel")]
-        public string Rel { get; set; }
+        public string? Rel { get; set; }
 
         [JsonPropertyName("method")]
-        public string Method { get; set; }
+        public string? Method { get; set; }
+    }
+
+    public class PayPalWebhookEvent
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("event_type")]
+        public string? EventType { get; set; }
+
+        [JsonPropertyName("resource")]
+        public PayPalWebhookResource? Resource { get; set; }
+    }
+
+    public class PayPalWebhookResource
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+    }
+
+    public class PayPalWebhookVerificationResponse
+    {
+        [JsonPropertyName("verification_status")]
+        public string? VerificationStatus { get; set; }
     }
 }
